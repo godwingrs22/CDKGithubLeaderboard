ae65149e003ae008071d6cd212beeff77a3fa558
shikha372 <shikagg@amazon.com>
2025-03-11 23:44:51 -0700

    adding frontend js files

---
+++ frontend/cdk-leaderboard/src/App.js
@@ -0,0 +1,15 @@
+// Main App file
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CDK GitHub Leaderboard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
